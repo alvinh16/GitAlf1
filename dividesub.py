c2f512b770fae099213ae3a92d65ca2781c3b2cc
Alvin Ho <alvinho@yandex.com>
2018-02-17 03:54:34 +0800

    quotient function

---
+++ dividesub.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def quotient( x, y ):
+    return x//y
+
+numerator = int(28)
+denominator = int(7)
+result = quotient (numerator,denominator)
+
+print(str(numerator) + " divide by "+ str(denominator) + " equals " + str(result))
