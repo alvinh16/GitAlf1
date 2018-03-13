fe166e5dcf69950c899240d1487991ca9d6c9ef4
Alvin Ho <alvinho@yandex.com>
2018-03-14 01:58:22 +0800

    return 3rd power

---
+++ 3power.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def power(index):
+    thirdpower = int(index) ** 3
+    return (thirdpower)
+
+base = input ("what would u like 2 know the 3rd power of? ")
+
+print (base+" tp the power of 3 is "+ str(power(base)))
